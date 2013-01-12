cc18f9e3b58b742dfc7d944a57ab4fcad10ce952
Jeffrey Zhao <jeffz@live.com>
2013-01-13 01:03:52 +0800

    Equals and GetHashCode

--- src/NRopes/Rope.cs
+++ src/NRopes/Rope.cs
@@ -65,6 +65,31 @@
             return GetEnumerator();
         }
 
+        public override bool Equals(object obj) {
+            if (obj == null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            var rope = obj as Rope;
+            if (rope == null || Length != rope.Length) return false;
+
+            return this.SequenceEqual(rope);
+        }
+
+        public override int GetHashCode() {
+            var hash1 = 5381;
+            var hash2 = hash1;
+
+            var etor = GetEnumerator();
+            while (etor.MoveNext()) {
+                hash1 = ((hash1 << 5) + hash1) ^ etor.Current;
+
+                if (!etor.MoveNext()) break;
+                hash2 = ((hash2 << 5) + hash2) ^ etor.Current;
+            }
+
+            return hash1 + (hash2 * 1566083941);
+        }
+
         public override string ToString() {
             throw new NotImplementedException("Please override ToString() method in derived classes of Rope.");
         }
